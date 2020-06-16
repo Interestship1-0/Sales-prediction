21210cf1fd8c4478eaba13927ab186d5bf748758
Ghonia <hena.ghonia@ecolab.com>
2020-06-17 03:39:06 +0530

    first commit

---
+++ src/settings.py
@@ -0,0 +1,22 @@
+import dotenv
+import os
+
+
+project_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+dotenv_path = os.path.join(project_dir, '.env')
+dotenv.load_dotenv(dotenv_path)
+
+
+# Local datafiles
+data_folder = os.path.join(project_dir, 'data')
+raw_data = os.path.join(data_folder, 'raw')
+interim_data = os.path.join(data_folder, 'interim')
+external_data = os.path.join(data_folder, 'external')
+test_data = os.path.join(data_folder, 'test')
+processed_data = os.path.join(data_folder, 'processed')
+logg= os.path.join(project_dir, 'logs')
+#one_drive = os.path.realpath(r'C:\Users\hayesgr\OneDrive - Ecolab\FRS\foodsafety\Category-Question Maps')
+
+
+# Models
+saved_models = os.path.join(project_dir, 'models')
